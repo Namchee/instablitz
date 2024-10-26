45651f93853144d7937557abaf82d1e9ca0e48a6
Cristopher <cristophernamchee12@gmail.com>
2024-10-26 21:06:59 +0700

    chore: remove original template config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { defineExtension } from 'reactive-vscode'
-import { window } from 'vscode'
+import { window, workspace } from 'vscode'
 
-const { activate, deactivate } = defineExtension(() => {
+const { activate, deactivate } = defineExtension(async () => {
   window.showInformationMessage('Hello')
+
+  const files = await workspace.findFiles('**/*.*', 'node_modules');
+  for (const file of files) {
+    console.log(file.path);
+    window.showInformationMessage(file.path);
+  }
 });
 
 export { activate, deactivate }
