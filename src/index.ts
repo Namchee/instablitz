838421ddb19a84e362986f9ab99572b5e67d8ab0
Cristopher <cristophernamchee12@gmail.com>
2024-10-27 22:30:10 +0700

    feat: add stackblitz POST template

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import * as vscode from 'vscode';
 
+const PAYLOAD_TEMPLATE = `<html lang="en">
+<head>
+</head>
+
+<body>
+  <form id="mainForm" method="post" action="https://stackblitz.com/run" target="_self">
+    {content}
+  </form>
+
+  <script>
+    document.getElementById("mainForm").submit();
+  </script>
+</body>
+</html>`;
+
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     'extension.instablitz',
-    () => {
-      const ws = vscode.workspace.findFiles('**/*.*', 'node_modules');
+    async () => {
+      const name = vscode.workspace.name;
+
+      const files = await vscode.workspace.findFiles('**/*.*', 'node_modules');
+      const gitignore = files.find((file) => file.path.endsWith('.gitignore'));
     },
   );
 
