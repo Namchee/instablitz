c633f56997949930eca41431ae38cc6d2c84c4f0
Cristopher <cristophernamchee12@gmail.com>
2024-10-28 21:59:45 +0700

    feat: experiment with filesystem

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,43 @@ const PAYLOAD_TEMPLATE = `<html lang="en">
 </body>
 </html>`;
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  const files = await vscode.workspace.findFiles('**/*.*', 'node_modules');
+  const packageJson = files.find((file) => file.path.endsWith('package.json'));
+  if (!packageJson) {
+    vscode.window.showErrorMessage(
+      'This extension only works on npm-managed projects',
+    );
+    return;
+  }
+
+  const rawMetadata = await vscode.workspace.fs.readFile(packageJson);
+  const { dependencies } = JSON.parse(rawMetadata.toString());
+  console.log(dependencies);
+
+  const name = vscode.workspace.name;
+
   const disposable = vscode.commands.registerCommand(
     'extension.instablitz',
     async () => {
+      const files = await vscode.workspace.findFiles('**/*.*', 'node_modules');
+      const packageJson = files.find((file) =>
+        file.path.endsWith('package.json'),
+      );
+      if (!packageJson) {
+        vscode.window.showErrorMessage(
+          'This extension only works on npm-managed projects',
+        );
+        return;
+      }
+
+      const rawMetadata = await vscode.workspace.fs.readFile(packageJson);
+      const { dependencies } = JSON.parse(rawMetadata.toString());
+      console.log(dependencies);
+
       const name = vscode.workspace.name;
 
-      const files = await vscode.workspace.findFiles('**/*.*', 'node_modules');
-      const gitignore = files.find((file) => file.path.endsWith('.gitignore'));
+      // const gitignore = files.find((file) => file.path.endsWith('.gitignore'));
     },
   );
 
